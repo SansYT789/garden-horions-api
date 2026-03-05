fe3a31c777fa5b00a359aff05ff34eef74b7758f
SansYT <128694801+SansYT789@users.noreply.github.com>
2026-03-05 18:54:21 +0700

    Create sync.js

---
+++ sync.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
+const FormData = require("form-data");
+
+const API = "https://YOUR-RAILWAY-URL";
+
+const BASE = "./local_assets/garden_horizons";
+
+const folders = ["gear", "plant", "weather"];
+
+async function sync() {
+
+  console.log("Clearing server...");
+
+  await axios.post(API + "/clear");
+
+  const form = new FormData();
+
+  folders.forEach(folder => {
+
+    const dir = path.join(BASE, folder);
+
+    const files = fs.readdirSync(dir);
+
+    files.forEach(file => {
+
+      const filePath = path.join(dir, file);
+
+      form.append("files", fs.createReadStream(filePath), folder + "/" + file);
+
+    });
+
+  });
+
+  console.log("Uploading images...");
+
+  await axios.post(API + "/upload", form, {
+    headers: form.getHeaders(),
+    maxBodyLength: Infinity
+  });
+
+  console.log("Upload complete");
+
+}
+
+sync();
